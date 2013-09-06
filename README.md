43ab1aba623b53873fa216b54956e94e941b42db
Richard Lucas <rlucas11@yahoo.com>
2013-09-06 10:23:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+sweet
+=====
+
+Replication of Meier, B. P, Moeller, S. K., Riemer-Peltz, M., & Robinson, M. D. (2012). Sweet taste Preferences and experiences predict prosocial inferences, personalities, and behaviors. _Journal of Personality and Social Psychology, 102,_ 163-174.
+
+In this study, we attempted a large-sample exact replication of Study 2 of Meier et al. (2012). In the original study, 55 participants (27 Male) reported on their personality using Goldberg's (1999) 50-item IPIP measure. In addition, participants reported their liking for a variety of foods classified as being bitter, salty, sour, spicy, or sweet. They found that Agreeableness correlated .36 with liking of sweet-tasting food. 
+
+We administered the same personality and taste-preference measures to 414 participants (122 Male). We replicated the direction of the effect from their study, with a correlation of .22 [.12 to .31]. Our correlation was, however, significantly smaller than the .36 value obtained in the original study. 
+
+A standard procedure in our lab is to use directed questions (asking participants to select a specific response option for one or more questions) as a quality check. Of the initial 414 participants, 85 failed the quality check. 
---
+++ sweet.R
@@ -0,0 +1,35 @@
+## Clean SPSS File for Public File
+## Variables removed:
+## Personality items, SWLS, Affect, Specific taste items, Loneliness items,
+## Bathing items from Bargh and Shalev replication, race/ethnicity (to prevent identification),
+## Age, Year in college, Religion, English as first language
+
+require("foreign")
+sweet <- read.spss("TASTEFILE.sav", use.value.labels=FALSE)
+sweet <- as.data.frame(sweet)
+sweet <- sweet[,c("ipipe","ipipa","ipipc","ipipn","ipipo","bitter","salty","sour","spicey","sweet",
+                  "filter_.","gender")]
+
+write.csv(sweet, "sweet.csv", row.names=F)
+
+## Analyses:
+sweet <- read.csv("sweet.csv")
+table(sweet$gender) # 1 = Female; 2 = Male
+table(sweet$filter_.) # Quality check: 1 = pass; 0 = failed
+
+## Replication Analyses:
+cor(sweet[,c("ipipe","ipipa","ipipc","ipipn","ipipo")], sweet[,c("bitter","salty","sour","spicey","sweet")],
+    use="pairwise.complete.obs")
+cor.test(~ ipipa + sweet, data=sweet)
+
+
+## Replication Analyses removing those who did not pass quality check:
+cor(sweet[which(sweet$filter_.==1),c("ipipe","ipipa","ipipc","ipipn","ipipo")],
+    sweet[which(sweet$filter_.==1),c("bitter","salty","sour","spicey","sweet")],
+    use="pairwise.complete.obs")
+cor.test(~ ipipa + sweet, data=sweet, subset=(filter_.==1))
+
+## Controlling for gender
+require("ppcor")
+pcor(sweet[complete.cases(sweet[,c("ipipa","sweet","gender")]),c("ipipa","sweet","gender")])
+genderModel <- lm(sweet ~ ipipa + gender, data=sweet)
